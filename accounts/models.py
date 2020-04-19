bed64b61443638a7a8b2ffd8fc4d9a0604b6284b
billybimbob <noahluvsmilkduds@gmail.com>
2020-04-18 22:08:08 -0500

    some urls and html start

--- accounts/models.py
+++ accounts/models.py
@@ -3,12 +3,20 @@ from django.db import models
 # Create your models here.
     
 class User(models.Model):
-    credit_card   = models.CharField(max_length=20, primary_key=True)
+    credit_card   = models.CharField(max_length=20, unique=True)
     license_plate = models.CharField(max_length=9)
+
+    def __str__(self):
+        return f'guest with plate: {self.license_plate}'
 
 class Member(models.Model):
     user       = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     first_name = models.CharField(max_length=20)
     last_name  = models.CharField(max_length=256)
+    email      = models.CharField(max_length=20)
+    password   = models.CharField(max_length=20)
     start_date = models.DateField(auto_now_add=True)
     end_date   = models.DateField()
+
+    def __str__(self):
+        return f'member {self.first_name} {self.last_name}'
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse(f'This will be the login to a user page')
+
+def details(request, user_id):
+    return HttpResponse(f'Member profile for {user_id}', user_id)
