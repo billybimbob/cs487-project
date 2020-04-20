aa1242fb4bc0bd7a379f3e652ef2a56c053f8159
Your Name <noahluvsmilkduds@gmail.com>
2020-04-20 13:50:46 -0500

    using sessions

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from .models import Member, User
-from .forms import LoginForm, MemberForm, GuestForm
+from . import forms
 
 # Create your views here.
 
@@ -10,7 +10,7 @@ def login(request):
     context = {
         'title': 'Parking Login',
         'login_page': "active",
-        'form': LoginForm(),
+        'form': forms.LoginForm(),
         'method': "get",
         'target': 'validate',
         'submit': 'Login',
@@ -21,7 +21,7 @@ def new_account(request):
     context = {
         'title': 'Create a New Membership',
         'create_page': "active",
-        'form': MemberForm(),
+        'form': forms.MemberForm(),
         'method': "post",
         'target': 'validate',
         'submit': 'Sign Up'
@@ -29,20 +29,27 @@ def new_account(request):
     return render(request, 'accounts/signin.html', context)
 
 def mem_validate(request):
+    mem = None
     if request.method == 'POST':
-        form = MemberForm(request.POST)
+        form = forms.MemberForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = User()
+            user.save()
+            mem = form.save()
+            mem.user = user
             print('added new member')
     else: #get
-        form = LoginForm(request.GET)
+        form = forms.LoginForm(request.GET)
         if form.is_valid():
             data = form.cleaned_data
             try:
-                m = getattr(Member, "objects").get(email=data["email"], password=data["password"])
-                print(f'found member {m}')
+                mem = getattr(Member, "objects").get(email=data["email"], password=data["password"])
             except getattr(Member, "DoesNotExist"):
                 print('invalid credentials')
+
+    if mem is not None:
+        request.session['id'] = mem.user.uid
+        print(f'successfully logged in as {request.session["id"]}')
 
     return HttpResponse(f'check signin option')
 
@@ -50,7 +57,7 @@ def guest(request):
     context = {
         'title': 'Guest Confirmation',
         'guest_page': "active",
-        'form': GuestForm(),
+        'form': forms.GuestForm(),
         'method': "post",
         'target': 'guest_cont',
         'submit': 'Continue',
@@ -60,6 +67,7 @@ def guest(request):
 def guest_signin(request):
     new_guest = User()
     new_guest.save()
+    request.session['id'] = new_guest.uid
     return HttpResponse(f'signed in as guest {new_guest.uid}')
 
 
